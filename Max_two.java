84178f2e7d1fa78af8e2d7072cb7b592195594b5
HM Azijul <62802022+HM-Azijul@users.noreply.github.com>
2021-03-17 17:19:23 +0600

    Add files via upload

---
+++ Max_two.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Max_two
+{
+public static void main(String[] args)
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter First Number:");
+a = sc.nextInt();
+
+System.out.println("Enter Second Number:");
+b = sc.nextInt();
+
+if(a>b)
+System.out.println("Max Number = "+a);
+else
+System.out.println("Max Number = "+b);
+}
+}
